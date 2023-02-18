bbf602de7037239154df7c6453786861f0af269e
boyhoanghoi <125749207+boyhoanghoi@users.noreply.github.com>
2023-02-18 23:46:19 +0700

    Add files via upload

---
+++ Bai1.java
@@ -0,0 +1,30 @@
+package javaapplication4;
+
+import java.util.Scanner;
+
+public class Bai1
+{
+	public static void main(String[] args)
+	 {
+		Scanner input = new Scanner(System.in);
+		double x;
+
+		System.out.println("Nhap so a bat ky: "); float a = input.nextFloat();
+		System.out.println("Nhap so b bat ky: "); float b = input.nextFloat();
+		if (a==0)
+		if(b==0)
+		{
+
+			System.out.println("Phuong trinh vo so nghiem");
+		}
+		else
+		{
+			System.out.println("Phuong trinh vo nghiem");
+		}
+		else
+		{
+			x= -b/a;	
+			System.out.println("Phuong trinh bac nhat co nghiem x = " + x);
+		}
+	}
+}
